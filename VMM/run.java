541fb481457229f426374e94b654b1f77601494f
danielson <dadjocy21@students.desu.edu>
2023-12-31 23:51:28 -0500

    page and offset gained

--- VMM/run.java
+++ VMM/run.java
@@ -10,13 +10,27 @@ public class run {
         File addresses = new File("/Users/dadjocy21students.desu.edu/Desktop/dev-me/AD_VMM_java/addresses.txt");
         Scanner scan = new Scanner(addresses);
         int i = 0;
+        page_table page_test = new page_table();
 
         while(scan.hasNextLine()){
-            System.out.println(i + ": " +scan.nextLine());
+            int address = Integer.valueOf(scan.nextLine()); // takes each individual address
+            int page_num = (address >> 8) & 0x00FF;
+            int offset = address & 0x00FF;
+
+            /*
+            Page Number = (logical_address >> 8) & 0x00FF
+            Offset = logical_address & 0x00FF
+             */
+            System.out.println(i + ": " +address+" - page: "+page_num+" - offset: "+offset);
+
+            System.out.println(page_test.table_lookup(page_num));
+
             i++;
         }
+
+
         /*
-        page_table page_test = new page_table();
+
         for (int i = 0; i<256; i++){
             System.out.println(page_test.getTable(i).getFrame_number());
             System.out.println(page_test.getTable(i).getValid());
