24ca90a406a954c564db0d4c196a0f157f057dec
danielson <dadjocy21@students.desu.edu>
2024-01-01 16:39:01 -0500

    update working

--- VMM/run.java
+++ VMM/run.java
@@ -1,4 +1,6 @@
 package VMM;
+import VMM.hashes.TLB_hash;
+
 import java.util.*;
 import java.io.File;
 import java.util.Scanner;
@@ -9,23 +11,44 @@ public class run {
     public static void main(String[] args) throws Exception {
         File addresses = new File("/Users/dadjocy21students.desu.edu/Desktop/dev-me/AD_VMM_java/addresses.txt");
         Scanner scan = new Scanner(addresses);
-        int i = 0;
+        //These two lines read the address file
+        int i = 1;
+
         page_table page_test = new page_table();
+        TLB translator = new TLB();
+        //Initializes the page table and TLB
 
         while(scan.hasNextLine()){
             int address = Integer.valueOf(scan.nextLine()); // takes each individual address
             int page_num = (address >> 8) & 0x00FF;
             int offset = address & 0x00FF;
 
-            /*
-            Page Number = (logical_address >> 8) & 0x00FF
-            Offset = logical_address & 0x00FF
-             */
-            System.out.println(i + ": " +address+" - page: "+page_num+" - offset: "+offset);
+            System.out.print(i + ": " +address+" - page: "+page_num+" - offset: "+offset);
+            int TLB_result = translator.TLB_search(page_num);
+            if (TLB_result == -1){ //TLB miss
+                System.out.print(" TLB miss");
+                int page_result = page_test.table_lookup(page_num);
+                if (page_result == -1){ //page fault
+                    //THIS WILL BE PHYSICAL MEMORY
+                    int newFrame = page_num%10;
+                    page_test.table_update(page_num, newFrame);
+                    translator.TLB_update(page_num, newFrame);
+                    System.out.print(" page fault");
+                }
+                else{// page hit
+                    translator.TLB_update(page_num, page_result);
+                    System.out.println(page_result);
+                }
+            }
+            else{// TLB hit
+                System.out.println(TLB_result);
+            }
 
-            System.out.println(page_test.table_lookup(page_num));
+
+            //System.out.println(page_result);
 
             i++;
+            System.out.println();
         }
 
 
