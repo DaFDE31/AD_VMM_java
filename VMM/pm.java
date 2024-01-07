79d2c775a9e3ba28fad1305528aa44c97e4f8d16
danielson <dadjocy21@students.desu.edu>
2024-01-07 15:12:01 -0500

    code cleaned

--- VMM/pm.java
+++ VMM/pm.java
@@ -12,22 +12,8 @@ public class pm {
         file = new FileInputStream("/Users/dadjocy21students.desu.edu/Desktop/dev-me/AD_VMM_java/BACKING_STORE.bin");
         physical = new Integer[256*256];
 
-        int read = file.read(binary);
+        file.read(binary);
         frameNum = 0;
-
-
-        /*/////////////////////////////////////////////
-        for (int i = 0; i < binary.length; i++){
-            System.out.println(i+": " +binary[i]);
-        }
-        System.out.println(read);
-        /////////////////////////////////////////////*/
-
-        //For testing
-        for (int bite = 0; bite < 256*256; bite++){
-            physical[bite] =  bite;
-        }
-        /////////////////////
     }
     public int grab(int frame, int offset){//This function should be used to grab the value at this offset
         return physical[frame*256+offset];
@@ -43,11 +29,5 @@ public class pm {
         return frameNum-1;//returns that frame number into the page table
 
     }
-    /*public void print(){
-        for (Integer hi : physical){
-            System.out.println("hello: "+ hi);
-        }
-    }
-     */
 
 }
--- VMM/run.java
+++ VMM/run.java
@@ -12,7 +12,6 @@ public class run {
         File addresses = new File("/Users/dadjocy21students.desu.edu/Desktop/dev-me/AD_VMM_java/addresses.txt");
         Scanner scan = new Scanner(addresses);
         //These two lines read the address file
-        int i = 1;
 
         page_table page_test = new page_table();
         TLB translator = new TLB();
@@ -24,47 +23,29 @@ public class run {
             int page_num = (address >> 8) & 0x00FF;
             int offset = address & 0x00FF;
 
-            System.out.print(i + ": " +address+" - page: "+page_num+" - offset: "+offset);
             int TLB_result = translator.TLB_search(page_num);
+
             if (TLB_result == -1){ //TLB miss
-                System.out.print(" TLB miss");
+                System.out.print("TLB miss");
                 int page_result = page_test.table_lookup(page_num);
                 if (page_result == -1){ //page fault
+
                     //THIS WILL BE PHYSICAL MEMORY
                     int newFrame = hard.update(page_num);
                     page_test.table_update(page_num, newFrame);
-                    translator.TLB_update(page_num, newFrame);
-                    TLB_result = newFrame;
+                    page_result = newFrame;
                     System.out.print(" page fault");
                 }
-                else{// page hit
-                    translator.TLB_update(page_num, page_result);
-                    TLB_result = page_result;
-                    System.out.println(" Frame: "+ page_result);
-                }
+                translator.TLB_update(page_num, page_result);
+                TLB_result = page_result;
+                    //System.out.println("Page: "+page_num+" Frame: "+ TLB_result);
+
             }
-            else{// TLB hit
-                //System.out.print(" HIT: "+ hard.grab(TLB_result, offset));
-                System.out.println(" Frame: "+ TLB_result);
-            }
-
-
-            //System.out.println(page_result);
-
-            i++;
-            System.out.printf("\nADDRESS: (%d,%d) %d,%d,%d", page_num, offset, address, TLB_result*256+offset, hard.grab(TLB_result, offset));
+            System.out.println();
+            System.out.println("Page: "+page_num+" Frame: "+ TLB_result);
+            System.out.printf("ADDRESS: (%d,%d) %d,%d,%d\n", page_num, offset, address, TLB_result*256+offset, hard.grab(TLB_result, offset));
             System.out.println();
         }
-        //hard.print();
 
-
-        /*
-
-        for (int i = 0; i<256; i++){
-            System.out.println(page_test.getTable(i).getFrame_number());
-            System.out.println(page_test.getTable(i).getValid());
-        }
-
-         */
     }
 }
